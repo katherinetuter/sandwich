a2e6aebb30fcc3a02724169f79352c728450085e
katherine tuter <katherinetuter@gmail.com>
2017-02-20 21:19:39 -0600

    created IIFE & added ingredients variable

---
+++ javascripts/sandwichMaker.js
@@ -0,0 +1,20 @@
+(function (){
+	var ingredients = {
+	"turkey": 0.90, 
+	"pepperoni": 1.50, 
+	"salami": 1.00,
+	"ham": .75,
+	"sourdough": 3.00,
+	"wheat": 3.00,
+	"italian": 3.00,
+	"jalapenos": 0.20,
+	"lettuce": 0.01,
+	"onions": 0.10,
+	"american": 0.50,
+	"provolone": 0.65,
+	"swiss": 0.70,
+	"oil": 0.20,
+	"mayo": 0.20,
+	"mustard": 0.16 }
+})();
+
